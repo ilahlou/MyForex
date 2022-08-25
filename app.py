824f6999fcd59e7f3010be1a2b1517cead3cf97a
ilahlou <62577226+ilahlou@users.noreply.github.com>
2022-08-25 16:29:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-#def hello():
-#    return "Hello, Ilahlou"
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+def hello():
+    return "Hello, Ilahlou"
 
-symbol = 'TQQQ'
-import yfinance as yf
-df=yf.download(tickers=symbol, period="50d", interval="1D")
-
-#Plot
-price_X = np.arange(df.shape[0])
-
-plt.xlabel('Days')
-plt.ylabel('Price')
-plt.plot(price_X, df['Close'], label='Closing Prices')
-plt.legend()
-plt.show()
