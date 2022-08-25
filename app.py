78cecb86ef44724ac1102639de86071799a8429b
ilahlou <62577226+ilahlou@users.noreply.github.com>
2022-08-25 16:43:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Ilahlou"
-symbol = 'AUDCAD=X'
-import yfinance as yf
-df=yf.download(tickers=symbol, period="5d", interval="1D")
-df
+    return "AUDCAD=X"
+
