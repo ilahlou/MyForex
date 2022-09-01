938e70af017d83203caf8f4f738bba753a5a5b44
ilahlou <62577226+ilahlou@users.noreply.github.com>
2022-09-01 10:44:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#@app.route("/")
-#def index():
-#    return "Congratulations, it's a web app!"
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app!"
 
-@app.route("/<int:celsius>")
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    fahrenheit = float(celsius) * 9 / 5 + 32
-    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-    return str(fahrenheit)
+#@app.route("/<int:celsius>")
+#def fahrenheit_from(celsius):
+#    """Convert Celsius to Fahrenheit degrees."""
+#    fahrenheit = float(celsius) * 9 / 5 + 32
+#    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+#    return str(fahrenheit)
 
-value = input("What is the Value in Fahrenheit? ")
-fahrenheit_from(value)
+#value = input("What is the Value in Fahrenheit? ")
+#fahrenheit_from(value)
