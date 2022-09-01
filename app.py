bdf889814aed18ba5724f99ecc271ecf6165acb3
ilahlou <62577226+ilahlou@users.noreply.github.com>
2022-09-01 09:41:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from flask import Flask
-import numpy as np
-import streamlit as st  # pip install streamlit
-import pandas as pd  # pip install pandas
-import plotly.express as px  # pip install plotly-express
-import seaborn as sns
-import importlib_metadata
 
 app = Flask(__name__)
 
 #@app.route("/")
-#def hello():
-#    return "Pair: AUDCAD=X"
-
 #def index():
 #    return "Congratulations, it's a web app!"
 
@@ -22,5 +13,5 @@ def fahrenheit_from(celsius):
     fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
     return str(fahrenheit)
 
-#if __name__ == "__main__":
-#    app.run(host="127.0.0.1", port=8080, debug=True)
+value = input("What is the Value in Fahrenheit? ")
+fahrenheit_from(value)
