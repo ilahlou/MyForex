2599a2a7bb8d05a5cdaf795cf1a0fb882190ce65
ilahlou <62577226+ilahlou@users.noreply.github.com>
2022-09-01 08:55:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ import numpy as np
 import streamlit as st  # pip install streamlit
 import pandas as pd  # pip install pandas
 import plotly.express as px  # pip install plotly-express
-import base64  # Standard Python Module
-from io import StringIO, BytesIO  # Standard Python Module
 import seaborn as sns
-import os
 import importlib_metadata
 
 app = Flask(__name__)
